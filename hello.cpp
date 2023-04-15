6590087d1d874c1fc84b59d08b64fa832ce7d03c
SHUBHAM SINGH <128099554+SonaBabulover1@users.noreply.github.com>
2023-04-15 20:23:27 +0530

    CODE_PRATICE

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//using namespace std;
+int main()
+{
+    int a = 10, b=20,sum ; 
+    sum  = a+b ; 
+    using namespace std;
+    cout<< sum;
+    cout<<"\n";
+    // float = 3.4; // this is a comment in c++ language .. single line comment 
+    return 0;
+}
